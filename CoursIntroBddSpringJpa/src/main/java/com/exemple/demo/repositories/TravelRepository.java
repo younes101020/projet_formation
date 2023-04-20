537d4b0053ed6739080ce68024bcb4349c13215c
andresz64 <andre.szetela@gmail.com>
2023-04-20 18:56:40 +0200

    fix conflicts

--- CoursIntroBddSpringJpa/src/main/java/com/exemple/demo/repositories/TravelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemple.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exemple.demo.entities.Travel;
-
-public interface TravelRepository extends JpaRepository<Travel, Long>{
-
-}
